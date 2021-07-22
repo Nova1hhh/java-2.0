553688b07a9d2025facded291e3b3d3b3d0bbbeb
Nikolay <nikob95@mail.ru>
2021-07-22 03:14:08 +0300

    calculator v1.3 (задание 3)

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,8 +5,27 @@ import java.util.Scanner;
  */
 
 public class Calculator {
-    public static void main(String[] args) {
-        double val1, val2, result;
+    /**
+     * @param val1 левый операнд
+     * @param val2 правый операнд
+     * @return возвращает сумму введенных значений
+     * @see <a href=url>https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5</a>
+     */
+    public double addition(double val1, double val2) {
+        return val1 + val2;
+    }
+    public double subtraction(double val1, double val2) {
+        return val1 - val2;
+    }
+    public double multiplication(double val1, double val2) {
+        return val1 * val2;
+    }
+    public double division (double val1, double val2) {
+        return val1 / val2;
+    }
+
+    public void result(){
+        double val1, val2;
         Scanner scanner = new Scanner(System.in);
         // Считаем числа с консоли
         System.out.println("Введите 1е число");
@@ -17,31 +36,16 @@ public class Calculator {
         String operation;
         System.out.println("Выберите операцию: +, -, /, *");
         operation = scanner.next();
-        switch (operation) {
-            case "+" -> System.out.printf("Сумма равна %.4f", addition(val1, val2));
-            case "-" -> System.out.printf("Разность равна %.4f", subtraction(val1, val2));
-            case "*" -> System.out.printf("Произведение равно %.4f", multiplication(val1, val2));
-            case "/" -> System.out.printf("Деление равно %.4f", division(val1, val2));
+        if (val2 != 0) {
+            switch (operation) {
+                case "+" -> System.out.printf("Сумма равна %.4f", addition(val1, val2));
+                case "-" -> System.out.printf("Разность равна %.4f", subtraction(val1, val2));
+                case "*" -> System.out.printf("Произведение равно %.4f", multiplication(val1, val2));
+                case "/" -> System.out.printf("Частное равно %.4f", division(val1, val2));
+            }
+        } else {
+            System.out.println("К сожалению, этот калькулятор не умеет делить на 0 =(\nПопобобуйте еще раз");
+            result();
         }
     }
-
-    /**
-     * @param val1 левый операнд
-     * @param val2 правый операнд
-     * @return возвращает сумму введенных значений
-     * @see <a href=url>https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5</a>
-     */
-    public static double addition(double val1, double val2) {
-        return val1 + val2;
-    }
-    public static double subtraction(double val1, double val2) {
-        return val1 - val2;
-    }
-    public static double multiplication(double val1, double val2) {
-        return val1 * val2;
-    }
-    public static double division (double val1, double val2) {
-        return val1 / val2;
-    }
-
 }
